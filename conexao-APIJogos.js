d5e0106cdf0608198532cb7a3cc61eb2fefdc03a
Nikolas <nikolasvictortola@gmail.com>
2024-05-17 11:46:40 -0300

    Teste local API

--- conexao-APIJogos.js
+++ conexao-APIJogos.js
@@ -11,15 +11,4 @@ const carregaAPI_Jogos = async () => {
     }
 };
 
-const carregaAPI_idJogos = async (idJogador) => {
-
-    const respostaAPI = await fetch(`https://steam-api-seven.vercel.app/api/jogos`);
-    
-    if(respostaAPI.status == 200){
-
-        const dadosJogos = await respostaAPI.json();
-        console.log(dadosJogos);
-        return dadosJogos;
-        
-    }
-};
+carregaAPI_Jogos();
